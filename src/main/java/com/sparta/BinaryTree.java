7159768e835ebc295d7550f8de65066ec15021c2
Malek <malekbusari@gmail.com>
2022-09-13 00:34:25 +0100

    Add BinaryTree code

--- src/main/java/com/sparta/BinaryTree.java
+++ src/main/java/com/sparta/BinaryTree.java
@@ -2,4 +2,36 @@ package com.sparta;
 
 public class BinaryTree {
 
+    Node root;
+
+    public int[] binarySort(int[] arrayToSort){
+
+    }
+
+    private BinaryTree createBinaryTree(int[] arrayToSort) {
+        BinaryTree bt = new BinaryTree();
+        for (int i = 0; i < arrayToSort.length; i++){
+            bt.add(arrayToSort[i]);
+        }
+    }
+
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
 }
